a890272d48a82dbea4ec029a09e938fe8421b188
diiimmmaaas <dm.sokolov1994@gmail.com>
2022-06-06 12:49:12 +0300

    refactoring social network component

--- src/social-networks/SocialNetworks.js
+++ src/social-networks/SocialNetworks.js
@@ -1,21 +1,40 @@
 import React from 'react';
 import styles from './SocialNetworks.module.scss'
+import instagramIcon from '../assets/icons/instagram.png'
+import facebookIcon from '../assets/icons/facebook.png'
+import linkedinIcon from '../assets/icons/linkedin.png'
+import githubIcon from '../assets/icons/github.png'
+
 
 export const SocialNetworks = () => {
+
+    const instagram = {
+        backgroundImage: `url(${instagramIcon})`,
+    }
+    const facebook = {
+        backgroundImage: `url(${facebookIcon})`,
+    }
+    const linkedin = {
+        backgroundImage: `url(${linkedinIcon})`,
+    }
+    const github = {
+        backgroundImage: `url(${githubIcon})`,
+    }
+
     return (
         <div className={styles.socialNetworks}>
             <ul className={styles.socialNetworkIcons}>
                 <li>
-                    <a className={styles.facebook} href='#'>F</a>
+                    <a style={facebook} className={styles.facebook} href='#'></a>
                 </li>
                 <li>
-                    <a className={styles.instagram} href='#'>I</a>
+                    <a style={instagram} className={styles.instagram} href='#'></a>
                 </li>
                 <li>
-                    <a className={styles.linkedin} href='#'>Ln</a>
+                    <a style={linkedin} className={styles.linkedin} href='#'></a>
                 </li>
                 <li>
-                    <a className={styles.github} href='#'>Gh</a>
+                    <a style={github} className={styles.github} href='#'>git status</a>
                 </li>
             </ul>
         </div>
