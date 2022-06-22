23a7e45b79e5a2de012f80e1619a76a99362f145
diiimmmaaas <dm.sokolov1994@gmail.com>
2022-06-22 14:14:53 +0300

    add react-tsparticles

--- src/main/Main.js
+++ src/main/Main.js
@@ -2,11 +2,16 @@ import React from 'react';
 import styles from './Main.module.scss'
 import styleContainer from '../common/styles/Container.module.scss'
 import {SocialNetworks} from '../social-networks/SocialNetworks';
+import Particles from "react-tsparticles";
+import {particlesOptions} from "../common/particleOption/particleOption";
+
+
 
 
 export const Main = () => {
     return (
         <div className={styles.mainBlock}>
+            <Particles className={styles.particles} options={particlesOptions}/>
             <div className={styleContainer.container}>
                 <div className={styles.greeting}>
                     <div className={styles.text}>
